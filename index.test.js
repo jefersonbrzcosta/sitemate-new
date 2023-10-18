1342211e20d1bec395d7e5cd9b96c6d3b3cfa323
Jeferson Costa <jefersonbrz.costa@gmail.com>
2023-10-17 23:12:30 -0300

    feat: init commit

--- index.test.js
+++ index.test.js
@@ -1,18 +1,61 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require("./index");
 
-describe('Time to words', () => {
-  it('Handles midnight', () => {
-    const timeInWords = convertTimeToWords('0:00');
-    expect(timeInWords).toBe('midnight');
+describe("Time to words", () => {
+  it("Handles midnight", () => {
+    const timeInWords = convertTimeToWords("0:00");
+    expect(timeInWords).toBe("midnight");
   });
 
-  it('Handles 30 - 8:30', () => {
-    const timeInWords = convertTimeToWords('8:30');
-    expect(timeInWords).toBe('half past eight');
+  it("Handles midday", () => {
+    const timeInWords = convertTimeToWords("12:00");
+    expect(timeInWords).toBe("midday");
   });
 
-  it('Handles times after 30 mins - 2:45', () => {
-    const timeInWords = convertTimeToWords('2:45');
-    expect(timeInWords).toBe('quarter to three');
+  it("Handles two o clock", () => {
+    const timeInWords = convertTimeToWords("2:00");
+    expect(timeInWords).toBe("two o' clock");
+  });
+
+  it("Handles two eleven", () => {
+    const timeInWords = convertTimeToWords("2:11");
+    expect(timeInWords).toBe("eleven past two");
+  });
+
+  it("Handles quarter past two", () => {
+    const timeInWords = convertTimeToWords("2:15");
+    expect(timeInWords).toBe("quarter past two");
+  });
+
+  it("Handles half past two", () => {
+    const timeInWords = convertTimeToWords("2:30");
+    expect(timeInWords).toBe("half past two");
+  });
+
+  it("Handles twenty seven to three", () => {
+    const timeInWords = convertTimeToWords("2:33");
+    expect(timeInWords).toBe("twenty seven to three");
+  });
+
+  it("Handles twenty to three", () => {
+    const timeInWords = convertTimeToWords("2:40");
+    expect(timeInWords).toBe("twenty to three");
+  });
+  it("Handles quarter to three", () => {
+    const timeInWords = convertTimeToWords("2:45");
+    expect(timeInWords).toBe("quarter to three");
+  });
+  it("Handles five to three", () => {
+    const timeInWords = convertTimeToWords("2:55");
+    expect(timeInWords).toBe("five to three");
+  });
+
+  it("Handles 30 - 8:30", () => {
+    const timeInWords = convertTimeToWords("8:30");
+    expect(timeInWords).toBe("half past eight");
+  });
+
+  it("Handles times after 30 mins - 2:45", () => {
+    const timeInWords = convertTimeToWords("2:45");
+    expect(timeInWords).toBe("quarter to three");
   });
 });
