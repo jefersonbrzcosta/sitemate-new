e3015bc2ed65182aa10467d558ffdd12a71c39f3
Jeferson Costa <jefersonbrz.costa@gmail.com>
2023-10-17 23:21:43 -0300

    refactor: export numerString

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
+const { numberString } = require("./numberString");
+
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
-  const numberString = {
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "thirteen",
-    14: "fourteen",
-    15: "quarter",
-    16: "sixteen",
-    17: "seventeen",
-    18: "eighteen",
-    19: "nineteen",
-    20: "twenty",
-    21: "twenty one",
-    22: "twenty two",
-    23: "twenty three",
-    24: "twenty four",
-    25: "twenty five",
-    26: "twenty six",
-    27: "twenty seven",
-    28: "twenty eight",
-    29: "twenty nine",
-    30: "half",
-  };
   // TODO: real code goes here!
   if (time === "0:00") {
     return "midnight";
@@ -40,23 +10,25 @@ function convertTimeToWords(time) {
     return "midday";
   }
 
-  const hourText = numberString[time.split(":")[0]];
-  const hour = time.split(":")[0];
-  const minutes = time.split(":")[1];
+  let hour = time.split(":")[0];
+  let minutes = time.split(":")[1];
 
   if (minutes === "00") {
-    return `${hourText} o' clock`;
+    hour = numberString[hour];
+    return `${hour} o' clock`;
   }
 
   if (Number(minutes) <= 30) {
-    return `${numberString[minutes]} past ${hourText}`;
+    minutes = numberString[minutes];
+    hour = numberString[hour];
+    return `${minutes} past ${hour}`;
   }
 
   if (Number(minutes) > 30) {
-    return `${numberString[60 - minutes]} to ${numberString[Number(hour) + 1]}`;
+    minutes = numberString[60 - minutes];
+    hour = numberString[Number(hour) + 1];
+    return `${minutes} to ${hour}`;
   }
-
-  return "half past eight";
 }
 
 module.exports = { convertTimeToWords };
